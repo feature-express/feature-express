7082e1c46bdf95a7de08d2843a3ea0ebdc504ab8
pawel <p.jankiewicz@gmail.com>
2023-08-24 10:20:36 +0200

    Bump version 0.0.4

--- fexpress-py/setup.py
+++ fexpress-py/setup.py
@@ -4,7 +4,7 @@ from setuptools_rust import RustExtension
 
 setup(
     name="fexpress",
-    version="0.0.3",
+    version="0.0.4",
     classifiers=[
         "Programming Language :: Python",
         "Programming Language :: Rust",
